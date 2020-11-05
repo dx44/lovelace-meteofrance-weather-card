370784abccd427300aaea4949868c77c8e7cee08
Axel Duviella <60935256+Axellum@users.noreply.github.com>
2020-11-05 15:17:39 +0100

    Update meteofrance-weather-card-editor.js

--- dist/meteofrance-weather-card-editor.js
+++ dist/meteofrance-weather-card-editor.js
@@ -111,7 +111,7 @@ export class MeteofranceWeatherCardEditor extends LitElement {
 
 
   get _detailEntity() {
-    return this._config.detail || "";
+    return this._config.detailEntity || "";
   }
 
 
